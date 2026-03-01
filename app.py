dd99ad2eec6d7aa376697b2ccbe9c9517cf1f2c6
Jashithrathod04 <akashi.hatake025@gmail.com>
2026-03-01 16:29:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,16 @@ st.set_page_config(
 
 
 
+st.markdown("""
+<style>
+body {
+    background-color: #0e1117;
+    color: white;
+}
+</style>
+""", unsafe_allow_html=True)
+
+
 
 import numpy as np
 
